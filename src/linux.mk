04e5776a8c0e4d262191e6c062db51c3059d4873
Jim Hayes <jhayes@sdsc.edu>
2014-08-18 15:22:26 -0700

    Avoid find warning message.

--- src/linux.mk
+++ src/linux.mk
@@ -54,7 +54,7 @@
 # @Copyright@
 #
 
-SRCDIRS = `find . -type d -maxdepth 1 \
+SRCDIRS = `find . -maxdepth 1 -type d \
 	-not -name CVS \
         -not -name geo-modules \
         -not -name r-geo \
