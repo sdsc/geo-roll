2070dc45bbb6b7dde11bf9e5f8473416c9f595f3
Jim Hayes <jhayes@sdsc.edu>
2014-08-19 13:46:25 -0700

    Significant cleanup of R module build process.  Rely on R to download module source instead of keeping copy in forge.  Move most of build environment setup out of separate script and into Makefile--script is now a short R program passed to Rscript.

--- src/r-geo/version.mk
+++ src/r-geo/version.mk
@@ -1,16 +1,7 @@
 NAME               = r-geo
 RELEASE            = 0
-PKGROOT            = /opt/R
 VERSION            = 1.0
 
 SRC_SUBDIR         = r-geo
-
-RGDAL_NAME         = rgdal
-RGDAL_VERSION      = 0.8-16
-RGDAL_SUFFIX       = tar.gz
-RGDAL_PKG          = $(RGDAL_NAME)_$(RGDAL_VERSION).$(RGDAL_SUFFIX)
-RGDAL_DIR          = $(RGDAL_PKG:%.$(SOURCE_SUFFIX)=%)
-
-TAR_GZ_PKGS        = $(RGEOS_PKG) $(RGDAL_PKG)
 
 RPM.EXTRAS         = AutoReq:No
