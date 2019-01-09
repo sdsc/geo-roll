18dff78b7e72e28352e298201e4ab41218cfdcd9
Jerry Greenberg <jpg@sdsc.edu>
2019-01-08 16:14:44 -0800

    APS-970 update geo.t to avoid merge conflicts

--- src/roll-test/version.mk
+++ src/roll-test/version.mk
@@ -1,6 +1,11 @@
+ifndef ROLLCOMPILER
+  ROLLCOMPILER = gnu
+endif
+COMPILERNAME := $(firstword $(subst /, ,$(ROLLCOMPILER)))
+
 NAME       = sdsc-geo-roll-test
 VERSION    = 1
-RELEASE    = 6
+RELEASE    = 7
 PKGROOT    = /root/rolltests
 
 RPM.EXTRAS = AutoReq:No
