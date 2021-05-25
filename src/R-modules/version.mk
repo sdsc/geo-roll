8ecc2a4bb157cb03fded5efa85627eafbf3ff577
Jerry Greenberg <jpg@sdsc.edu>
2021-05-24 19:09:47 -0700

    APS-1127 rgdal 1-5.23 and rgeos 0.5-5

--- src/R-modules/version.mk
+++ src/R-modules/version.mk
@@ -1,6 +1,6 @@
 NAME          = sdsc-geo-R-modules
-VERSION       = 5
-RELEASE       = 4
+VERSION       = 6
+RELEASE       = 0
 PKGROOT       = /opt/R/local/lib
 
 SRC_SUBDIR    = R-modules
@@ -8,13 +8,13 @@ R_MODULES = rgdal rgeos
 
 RGDAL_NAME    = rgdal
 RGDAL_SUFFIX  = tar.gz
-RGDAL_VERSION = 1.3-6
+RGDAL_VERSION = 1.5-23
 RGDAL_PKG     = $(RGDAL_NAME)_$(RGDAL_VERSION).$(RGDAL_SUFFIX)
 RGDAL_DIR     = $(RGDAL_NAME)
 
 RGEOS_NAME    = rgeos
 RGEOS_SUFFIX  = tar.gz
-RGEOS_VERSION = 0.4-2
+RGEOS_VERSION = 0.5-5
 RGEOS_PKG     = $(RGEOS_NAME)_$(RGEOS_VERSION).$(RGEOS_SUFFIX)
 RGEOS_DIR     = $(RGEOS_NAME)
 
